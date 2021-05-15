aea9a09d58c77667a9d4386116bf13515c9952ca
Joe Bouchard <joseph.c.bouchard-1@ou.edu>
2021-05-15 00:22:43 -0500

    Moved JS to static folder. Refactored to create output list within script

--- static/updator.js
+++ static/updator.js
@@ -1,8 +1,7 @@
-//var freqs = {};
 const notAlNumDashSpace = /[^a-zA-Z\-\s]/ig;
 
 function process() {
-	var rawText = document.getElementById('Input Text').value;
+	var rawText = document.getElementById('Input_Text').value;
 	document.getElementById('output').innerHTML = "Preprocessing... <br><b>DO NOT CLICK SUBMIT AGAIN</b> <br>It will slow down your results";
 	handleParagraph(rawText);
 	//const paragraphs = rawText.split("\n");
@@ -34,13 +33,29 @@ function handleParagraph(p){
 	if (numKeys > 5000){
 		document.getElementById('output').innerHTML += "<br><br><br>You have a large number of unique words. This may omit some to avoid timing out. If the time is >29 seconds, some words were omitted from the analysis";
 	}
+/*
+    var xhr = new XMLHttpRequest();  
+    xhr.open("POST", "/freqData");  
+    xhr.send(JSON.stringify(data)); 
+	var json_data = xhr.responseText;
+	console.log(json_data);
+	*/
 	fetch("/freqData", {
 		method: "POST", 
 		body: JSON.stringify(data)
-	}).then(res => res.json())
+	}).then(function(response) {
+		return response.json();
+	})
+	.then(function(jsonResponse) {
+		writeOut(jsonResponse);
+		// do something with jsonResponse
+	});
+	
+	/*.then(res => res.json())
 	.then(data => obj = data)
-	.then(() => writeOut(obj['text']));
+	.then(console.log(data));
 	//.then(() => addFreq(obj['freqs']));
+	*/
 }
 
 function addFreq(freq){
@@ -53,6 +68,26 @@ function addFreq(freq){
 		}
 	}
 }
+
+function addOut(t){
+	document.getElementById('output').innerHTML+=t;
+}
 function writeOut(data){
-	document.getElementById('output').innerHTML = data;
+	var newPage = "<ol>"
+	//document.getElementById('output').innerHTML = "<ol>"
+	for (key of Object.keys(data)){
+		console.log(key);
+		if (key != "TIME"){
+			newPage+="<li id="+key+"><a href="+data[key][2]+" target=\"_blank\">"+key+"</a>: ";
+			newPage+=data[key][0];
+			newPage+=" percent match with ";
+			newPage+=data[key][1]+" distinct words</li>";
+			
+		} else {
+			newPage+="</ol>";
+			newPage+="<br>";
+			newPage+="This took "+data[key]+" seconds";
+		}
+	}
+	document.getElementById('output').innerHTML = newPage
 }
